d8675e9eb26caf0577e9941b07fd6e33d8c84d58
René Fischer <rene.fischer@posteo.de>
2022-11-18 00:17:00 +0100

    Add frontend logic
    Problem:
    The frontend should communicate with the backend to be able to display
    the correct results.
    
    Solution:
    Add ajax call to communicate with the backend and add some smaller
    adjustments.

--- src/main/java/de/renfis/PictProcessorResource.java
+++ src/main/java/de/renfis/PictProcessorResource.java
@@ -2,13 +2,18 @@ package de.renfis;
 
 import java.io.IOException;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/pict")
 public class PictProcessorResource {
 
     @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
     public String pict(String pictInput) {
         return "Processed: " + pictInput;
     }
