24f20d9b3ef0b6a0564507d642cb3ae62ff45ea8
René Fischer <rene.fischer@posteo.de>
2022-11-18 12:15:35 +0100

    Use pict to generate test cases
    Problem:
    We want to execute Microsoft PICT to generate the test cases.
    
    Solution:
    Call pict in the PictProcessor to generate the result.

--- src/main/java/de/renfis/PictProcessor.java
+++ src/main/java/de/renfis/PictProcessor.java
@@ -1,12 +1,66 @@
 package de.renfis;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class PictProcessor {
 
-    public String process(String input) {
-        return "Processed: [" + input + "]";
+    private final Logger logger = LoggerFactory.getLogger(PictProcessor.class);
+
+    @ConfigProperty(name = "pict.executable.path")
+    String pictPath;
+
+    public String process(String input) throws IOException {
+        var path = createTempFile(input);
+        var output = executePict(path.toAbsolutePath().toString());
+
+        removeTempFile(path);
+        return output;
+    }
+
+    private Path createTempFile(String input) throws IOException {
+        var path = Files.createTempFile(null, ".pict");
+        Files.writeString(path, input);
+        return path;
+    }
+
+    private String executePict(String inputFile) throws IOException {
+        var processBuilder = new ProcessBuilder(pictPath, inputFile);
+        var process = processBuilder.start();
+
+        var errors = readLines(process, Process::errorReader);
+        return errors.isBlank()
+                ? readLines(process, Process::inputReader)
+                : errors;
+    }
+
+    private String readLines(
+            Process process,
+            Function<Process, BufferedReader> readerFn) throws IOException {
+        try (var reader = readerFn.apply(process)) {
+            return reader.lines()
+                    .collect(Collectors.joining("\n"));
+        }
+    }
+
+    private void removeTempFile(Path path) {
+        try {
+            Files.delete(path);
+            logger.debug("Temp file deleted: {}", path);
+        } catch (IOException e) {
+            logger.error("Could not delete tmp file.", e);
+        }
     }
 
 }
--- src/main/java/de/renfis/PictProcessorResource.java
+++ src/main/java/de/renfis/PictProcessorResource.java
@@ -1,5 +1,8 @@
 package de.renfis;
 
+import static org.jboss.resteasy.reactive.RestResponse.ResponseBuilder.ok;
+import static org.jboss.resteasy.reactive.RestResponse.Status.INTERNAL_SERVER_ERROR;
+
 import java.io.IOException;
 
 import javax.inject.Inject;
@@ -9,8 +12,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.resteasy.reactive.RestResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Path("/pict")
 public class PictProcessorResource {
+
+    private final Logger logger = LoggerFactory.getLogger(PictProcessorResource.class);
 
     @Inject
     PictProcessor pictProcessor;
@@ -18,8 +27,14 @@ public class PictProcessorResource {
     @POST
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.TEXT_PLAIN)
-    public String pict(String pictInput) {
-        return pictProcessor.process(pictInput);
+    public RestResponse<String> pict(String pictInput) {
+        try {
+            return ok(pictProcessor.process(pictInput), MediaType.TEXT_PLAIN_TYPE.withCharset("utf-8"))
+                    .build();
+        } catch (IOException e) {
+            logger.error("Error on processing pict input", e);
+            return RestResponse.status(INTERNAL_SERVER_ERROR, "Request could not be handled due to an internal error.");
+        }
     }
 
 }
