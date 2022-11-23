3ff16c29f40cd93f230a175ace8929264b0d6da1
René Fischer <rene.fischer@posteo.de>
2022-11-23 14:43:14 +0100

    Add test case amount to status bar
    Problem:
    We want to be able to see how many test cases were generated.
    
    Solution:
    Add test case count to status bar.

--- src/main/resources/META-INF/resources/js/main.js
+++ src/main/resources/META-INF/resources/js/main.js
@@ -1,11 +1,13 @@
 const inputField = document.getElementById('pict-input');
 const outputField = document.getElementById('pict-output');
 const progressStatusParagraph = document.getElementById('progress-status');
+const testAmountParagraph = document.getElementById('test-amount');
 
 const initialText = 'Bla Bla Bla...';
 inputField.value = initialText;
 outputField.value = '';
 progressStatusParagraph.textContent = 'Ready.';
+progressStatusParagraph.textContent = 'Test cases: 0';
 
 function clearInitialText() {
     if (inputField.value === initialText) {
@@ -24,9 +26,14 @@ function processPict() {
     xhttp.setRequestHeader('Content-Type', 'text/plain');
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4) {
-            outputField.value = this.status === 200
-                ? this.responseText
-                : "HTTP STATUS " + this.status + ": " + this.responseText;
+            if (this.status === 200) {
+                const linesWithoutHeadline = (this.responseText.match(/\n/g) || '').length;
+                testAmountParagraph.textContent = "Test cases: " + linesWithoutHeadline;
+                outputField.value = this.responseText;
+            } else {
+                testAmountParagraph.textContent = "Error " + this.status;
+                outputField.value = "HTTP STATUS " + this.status + ": " + this.responseText;
+            }
             progressStatusParagraph.textContent = 'Ready.';
         }
     };
